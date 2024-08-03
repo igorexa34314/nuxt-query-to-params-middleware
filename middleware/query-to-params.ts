dd6f7b73574d0fa62e417d80b1b31ba0fec31bfc
Igor Kononenko <igorkono343@gmail.com>
2024-08-03 17:49:01 +0300

    add page reload mode

--- middleware/query-to-params.ts
+++ middleware/query-to-params.ts
@@ -1,7 +1,7 @@
 // Redirect from 'type' query to 'type' param
 export default defineNuxtRouteMiddleware(to => {
 	const { type, ...q } = to.query;
-	console.log(to.params.type, type);
+	console.log('from middleware: param =', to.params.type, ';  query =', type);
 	if (typeof type === 'string') {
 		if (!to.params.type) {
 			return navigateTo({ ...to, query: q, params: { type } });
--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,18 @@
 		<NuxtLink :to="{ name: 'route-with-param-type', params: { type: 'foo' }, query: { type: 'baz' } }"
 			>Param = "foo", query = "baz"</NuxtLink
 		>
+		<br /><br />
+		<h2>With page reload</h2>
+		<NuxtLink to="/route-with-param" external>No param, no query</NuxtLink><br /><br />
+		<NuxtLink :to="{ name: 'route-with-param-type', params: { type: 'foo' } }" external
+			>Param = "foo", no query, page reload</NuxtLink
+		><br /><br />
+		<NuxtLink :to="{ name: 'route-with-param-type', query: { type: 'baz' } }" external
+			>No param, query = "baz", page reload</NuxtLink
+		><br /><br />
+		<NuxtLink :to="{ name: 'route-with-param-type', params: { type: 'foo' }, query: { type: 'baz' } }" external
+			>Param = "foo", query = "baz", page reload</NuxtLink
+		>
 	</div>
 </template>
 
