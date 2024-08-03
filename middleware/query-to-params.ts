21d87354e975a7ca6b4ca697e105c72b69097dad
Igor Kononenko <igorkono343@gmail.com>
2024-08-03 17:27:57 +0300

    fixing infinite redirect 2

--- middleware/query-to-params.ts
+++ middleware/query-to-params.ts
@@ -1,12 +1,11 @@
 // Redirect from 'type' query to 'type' param
 export default defineNuxtRouteMiddleware(to => {
 	const { type, ...q } = to.query;
-	if (!type) {
-		return;
-	} else if (!to.params.type && typeof type === 'string') {
-		console.log(to.params.type, type);
-
-		return navigateTo({ query: q, params: { type } });
+	console.log(to.params.type, type);
+	if (typeof type === 'string') {
+		if (!to.params.type) {
+			return navigateTo({ query: q, params: { type } });
+		}
+		return navigateTo({ query: q });
 	}
-	return navigateTo({ query: q });
 });
