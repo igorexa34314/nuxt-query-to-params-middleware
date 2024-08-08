bfaf2d87b77803cf8bf905111879c6a265e9a80a
Igor Kononenko <igorkono343@gmail.com>
2024-08-08 03:21:14 +0300

    clear query on client only

--- middleware/remove-type-query.ts
+++ middleware/remove-type-query.ts
@@ -1,5 +1,6 @@
 // Redirect from 'type' query to 'type' param
 export default defineNuxtRouteMiddleware(to => {
+	if (!import.meta.client) return;
 	const { type, ...q } = to.query;
 	console.log('from middleware: param =', to.params.type, ';  query =', type);
 	if (type) {
